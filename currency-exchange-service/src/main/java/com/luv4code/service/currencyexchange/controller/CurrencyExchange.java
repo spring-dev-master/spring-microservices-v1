a2acb15ab0bac664127215ae747a79dc04278b71
spring-dev-master <madhav.p.akv@outlook.com>
2022-11-08 12:18:37 +0530

    updating currency exchange application

---
+++ currency-exchange-service/src/main/java/com/luv4code/service/currencyexchange/controller/CurrencyExchange.java
@@ -0,0 +1,19 @@
+package com.luv4code.service.currencyexchange.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CurrencyExchange {
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+}
